0d000a742fe65fe941093f8f61dc95ffc0767a44
ItsElate <itselate123@gmail.com>
2023-11-26 22:48:57 -0500

    Added profile and profile fxml

--- src/main/java/com/example/toxic_pizzaui/controller/profileController.java
+++ src/main/java/com/example/toxic_pizzaui/controller/profileController.java
@@ -1,14 +1,28 @@
 package com.example.toxic_pizzaui.controller;
 
+import com.example.toxic_pizzaui.Tp_Utils;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class profileController {
     Button userButton;
 
     /*All the code related to the user profile page goes here: action buttons, order history, update information, etc.*/
 
+    @FXML
+    public void goBack(ActionEvent event) throws IOException {
+        Tp_Utils.changeScene(event, "MainPage.fxml");
+    }
+
+    @FXML
+    public void goToCart(ActionEvent event) throws IOException{
+        Tp_Utils.changeScene(event, "cart.fxml");
+    }
 }
